6a935f63ec672de7893df150086e013785f7b0c3
Vinícius Aiala <vinicius_aiala@outlook.com>
2021-08-13 14:00:24 -0300

    Added entities models

---
+++ src/main/java/br/com/opussoftware/desafio/model/Answer.java
@@ -0,0 +1,51 @@
+package br.com.opussoftware.desafio.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Answer {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String text;
+
+	@ManyToOne
+	private Question question;
+	@ManyToOne
+	private User author;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+}
---
+++ src/main/java/br/com/opussoftware/desafio/model/Question.java
@@ -0,0 +1,92 @@
+package br.com.opussoftware.desafio.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Question {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String text;
+	@Enumerated(EnumType.STRING)
+	private Status status = Status.NOT_ANSWERED;
+	@ManyToOne
+	private User author;
+	@ManyToOne
+	private Subject subject;
+	@OneToMany(mappedBy = "question")
+	private List<Answer> answers = new ArrayList<Answer>();
+
+	public Question(String title, String text, Subject subject) {
+		this.title = title;
+		this.text = text;
+		this.subject = subject;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+}
