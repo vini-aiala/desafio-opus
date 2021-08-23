1b08819833eb794eec4fbcdd7f674b92de021b66
Vinícius Aiala <vinicius_aiala@outlook.com>
2021-08-23 16:20:16 -0300

    Created form classes to represent user input

---
+++ src/main/java/br/com/opussoftware/desafio/controller/form/SubjectForm.java
@@ -0,0 +1,28 @@
+package br.com.opussoftware.desafio.controller.form;
+
+import br.com.opussoftware.desafio.model.Category;
+import br.com.opussoftware.desafio.model.Subject;
+import br.com.opussoftware.desafio.repository.SubjectRepository;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class SubjectForm {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String categoryName;
+
+    public Subject assemble() {
+        return new Subject(name, categoryName);
+    }
+
+    public Subject update(Long id, SubjectRepository subjectRepository) {
+        Subject subject = subjectRepository.getById(id);
+
+        subject.setName(this.name);
+        subject.setCategory(this.categoryName);
+
+        return subject;
+    }
+}
